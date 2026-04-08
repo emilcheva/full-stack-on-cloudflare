4c01ca268bd0bb4b6b976bf9ea514341ab5278d7
emilcheva <evgenia.milcheva@hotmail.com>
2026-04-08 20:18:26 +0300

    feat: create a sliding window of links clicks that is send as WS message to the client every 2s

--- apps/data-service/src/durable-objects/link-click-tracker.ts
+++ apps/data-service/src/durable-objects/link-click-tracker.ts
@@ -1,22 +1,35 @@
+import { deleteClicksBefore, getRecentClicks } from '@/helpers/durable-queries';
 import { DurableObject } from 'cloudflare:workers';
 import moment from 'moment';
 
 export class LinkClickTracker extends DurableObject {
 	sql: SqlStorage;
+	// keep track of most recent and oldest times so that we send a buffer of link data limited to this sliding window
+	// prevents bloating the sockets with hundreds of records
+	mostRecentOffsetTime: number = 0;
+	leastRecentOffsetTime: number = 0;
 
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
 		this.sql = ctx.storage.sql;
 
 		ctx.blockConcurrencyWhile(async () => {
+			const [leastRecentOffsetTime, mostRecentOffsetTime] = await Promise.all([
+				ctx.storage.get<number>('leastRecentOffsetTime'),
+				ctx.storage.get<number>('mostRecentOffsetTime'),
+			]);
+
+			this.leastRecentOffsetTime = leastRecentOffsetTime || this.leastRecentOffsetTime;
+			this.mostRecentOffsetTime = mostRecentOffsetTime || this.mostRecentOffsetTime;
+
 			this.sql.exec(`
-            CREATE TABLE IF NOT EXISTS geo_link_clicks (
-                latitude REAL NOT NULL,
-                longitude REAL NOT NULL,
-                country TEXT NOT NULL,
-                time INTEGER NOT NULL
-            )
-        `);
+                CREATE TABLE IF NOT EXISTS geo_link_clicks (
+                    latitude REAL NOT NULL,
+                    longitude REAL NOT NULL,
+                    country TEXT NOT NULL,
+                    time INTEGER NOT NULL
+                )
+            `);
 		});
 	}
 
@@ -31,6 +44,30 @@ export class LinkClickTracker extends DurableObject {
 			country,
 			time,
 		);
+		// wait for 2s to send a batch of data
+		const alarm = await this.ctx.storage.getAlarm();
+		if (!alarm) await this.ctx.storage.setAlarm(moment().add(2, 'seconds').valueOf());
+	}
+
+	async alarm() {
+		console.log('alarm');
+		const clickData = getRecentClicks(this.sql, this.mostRecentOffsetTime);
+
+		const sockets = this.ctx.getWebSockets();
+		for (const socket of sockets) {
+			// send the clickData clicks to the client
+			socket.send(JSON.stringify(clickData.clicks));
+		}
+
+		await this.flushOffsetTimes(clickData.mostRecentTime, clickData.oldestTime);
+		await deleteClicksBefore(this.sql, clickData.oldestTime);
+	}
+
+	async flushOffsetTimes(mostRecentOffsetTime: number, leastRecentOffsetTime: number) {
+		this.mostRecentOffsetTime = mostRecentOffsetTime;
+		this.leastRecentOffsetTime = leastRecentOffsetTime;
+		await this.ctx.storage.put('mostRecentOffsetTime', this.mostRecentOffsetTime);
+		await this.ctx.storage.put('leastRecentOffsetTime', this.leastRecentOffsetTime);
 	}
 
 	async fetch(_: Request) {
@@ -41,5 +78,9 @@ export class LinkClickTracker extends DurableObject {
 			status: 101,
 			webSocket: client,
 		});
+	}
+
+	webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean): void | Promise<void> {
+		console.log('client closed');
 	}
 }
